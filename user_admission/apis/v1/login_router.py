66c8a67676e5a1add30b145a77f385ac7822493d
rdmshine <rdmshine@gmail.com>
2022-06-09 15:47:19 +0900

    login router 주석 수정 및 login_service 파일 삭제.

--- user_admission/apis/v1/login_router.py
+++ user_admission/apis/v1/login_router.py
@@ -10,33 +10,24 @@ from user_admission.apis.v1.schemas.login_request import LoginRequest
 account = Router(tags=["MemberManagement"])
 
 
-# login page render router
-
-# @account.get("/", response=LoginResponse)
-# def get_login_page(request: HttpRequest) -> HttpResponse:
-#     return render(request, "login.html")
-
-# 경로가 ~ login/ 이고(/다음에 아무것도 없을때) get이면 위의 함수를 실행해라
-
-
 @account.get("/", url_name="login", response=LoginResponse)
 def get_login_page(request: HttpRequest) -> HttpResponse:
     return render(request, "login.html")
 
 
 @account.post("/", url_name="login", response=LoginResponse)
-def post_login(request: HttpRequest,loginrequest:LoginRequest = Form(...)) -> HttpResponse:
+def post_login(request: HttpRequest, loginrequest: LoginRequest = Form(...)) -> HttpResponse:
     username = loginrequest.username
-    #입력된 유저네임
+    # 입력된 유저네임
     password = loginrequest.password
-    #입력된 패스워드
+    # 입력된 패스워드
     user = auth.authenticate(request, username=username, password=password)
-    #유저네임과 패스워드가 일치하지않으면 None을 리턴하게된다
+    # 유저네임과 패스워드가 일치하지않으면 None을 리턴하게된다
     if user is not None:
-        #일치한다면
+        # 일치한다면
         auth.login(request, user)
-        #로그인
+        # 로그인
         return redirect("/")
     else:
         error = {"error": "이메일 또는 비밀번호를 확인해주세요."}
-        return render(request,"login.html",error)
+        return render(request, "login.html", error)
